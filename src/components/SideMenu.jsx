8a64c3de6fd20fedf1d934978cc0afea35d85e67
yybeltranr <yybeltranr@unal.edu.co>
2024-10-08 18:53:51 -0500

    Sin menú sombreado

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -10,85 +10,43 @@ import { useNavigate } from "react-router-dom";
 function SideMenu({ userData }) {
   const navigate = useNavigate();
   const [role, setRole] = useState(null);
-  const [selectedMenu, setSelectedMenu] = useState(null);  // Estado para el menú seleccionado
 
   useEffect(() => {
     const role = sessionStorage.getItem('rolname');
     setRole(role);
   }, []);
 
-  const handleClick = (menu, path) => {
-    setSelectedMenu(menu);
-    navigate(path);
-  };
-
   return (
-    <div className="col-2 sideMenu">
-      <nav>
-        <ul>
-          <IoIosMenu />
-          Menú
-        </ul>
-        <ul 
-          className="borderSec" 
-          onClick={() => handleClick('home', '/home')}
-          style={{
-            backgroundColor: selectedMenu === 'home' ? '#94B43B' : 'transparent',
-            color: selectedMenu === 'home' ? 'white' : 'black'
-          }}
-        >
-          <MdHomeFilled />
-          Home
-        </ul>
-        {(role === "admin") && (
-          <ul 
-            className="borderSec" 
-            onClick={() => handleClick('usuarios', '/crearUsuario')}
-            style={{
-              backgroundColor: selectedMenu === 'usuarios' ? '#94B43B' : 'transparent',
-              color: selectedMenu === 'usuarios' ? 'white' : 'black'
-            }}
-          >
+    <>
+      <div className="col-2 sideMenu">
+        <nav>
+          <ul>
+            <IoIosMenu />
+            Menú
+          </ul>
+          <ul className="borderSec" onClick={()=>{  navigate('/home');}}>
+            <MdHomeFilled />
+            Home
+          </ul>
+          {( role == "admin") && (
+          <ul className="borderSec"  onClick={()=>{  navigate('/crearUsuario');}}>
             <HiMiniUsers />
             Usuarios
+          </ul>)}        
+          <ul className="borderSec" onClick={()=>{  navigate('/tusEventos');}}>
+            <LuUserCheck />
+            Eventos
           </ul>
-        )}
-        <ul 
-          className="borderSec" 
-          onClick={() => handleClick('eventos', '/tusEventos')}
-          style={{
-            backgroundColor: selectedMenu === 'eventos' ? '#94B43B' : 'transparent',
-            color: selectedMenu === 'eventos' ? 'white' : 'black'
-          }}
-        >
-          <LuUserCheck />
-          Eventos
-        </ul>
-      </nav>
-      <nav>
-        <ul 
-          className="borderSec" 
-          onClick={() => handleClick('perfil', '/perfil')}
-          style={{
-            backgroundColor: selectedMenu === 'perfil' ? '#94B43B' : 'transparent',
-            color: selectedMenu === 'perfil' ? 'white' : 'black'
-          }}
-        >
-          <FaRegUserCircle />
-          Perfil
-        </ul>
-        <ul 
-          className="borderSec" 
-          onClick={() => {
-            sessionStorage.removeItem('token');
-            sessionStorage.removeItem('userId');
-            navigate('/');
-          }}
-        >
-          Cerrar sesión
-        </ul>
-      </nav>
-    </div>
+        </nav>
+        <nav>
+          <ul className="borderSec"  onClick={()=>{  navigate('/perfil');}}>
+            <FaRegUserCircle />
+            Perfil
+          </ul>
+          <ul  className="borderSec" onClick={()=>{sessionStorage.removeItem('token'); sessionStorage.removeItem('userId');  navigate('/')  }} >Cerrar sesión</ul>
+        </nav>
+      </div>
+    </>
   );
 }
 
